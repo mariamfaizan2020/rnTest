9ec7c00f59ba825f126e0d51b73bc275ff61cb86
mariam <>
2022-01-31 17:36:25 +0500

    designing of edit service screen

--- ios/rnTest/screens/EditServices.js
+++ ios/rnTest/screens/EditServices.js
@@ -5,25 +5,28 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 const EditServices = (props) => {
-    const [price,setPrice]=useState()
+    const [price,setPrice]=useState(props.navigation.state.params.price)
     console.log('propas',props.navigation.state.params.serviceId)
     const item=props.navigation.state.params
   return (
-    <View>
-      <TextInput  
+    <View style={{margin:10,padding:10}}>
+      <TextInput  style={styles.Button} 
+  
       value={item.service}
       editable={false}
       />
       <TextInput
-       value={item.price}
-      onChnageText={(text)=>setPrice(text)}>
+      style={{fontSize:16,color:'white',fontWeight:'bold',borderWidth:3,borderRadius:2,borderColor:'#969590',backgroundColor:'#969590'}}
+       value={price}
+       onChangeText={(text)=>setPrice(text)}>
+          
            </TextInput>
            <View style={{flexDirection:'row',margin:10,padding:10}}>
-           <TouchableOpacity>
-               <Text>EDIT SERVICE</Text>
+           <TouchableOpacity  style={{backgroundColor:'#a16281',borderColor:'#a16281',borderWidth:12,padding:5,margin:10}}>
+               <Text style={{alignSelf:'center',color:'white'}}>EDIT SERVICE</Text>
            </TouchableOpacity>
-           <TouchableOpacity>
-               <Text>DELETE SERVICE</Text>
+           <TouchableOpacity  style={{backgroundColor:'#a16281',borderColor:'#a16281',borderWidth:12,padding:5,margin:10}}>
+               <Text style={{alignSelf:'center',color:'white'}}>DELETE SERVICE</Text>
            </TouchableOpacity>
            </View>
          
@@ -34,4 +37,15 @@ const EditServices = (props) => {
 
 export default EditServices;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    Button:{
+        backgroundColor:'#a16281',
+        borderColor:'#a16281',
+        borderWidth:10,
+        alignSelf:'center',
+        marginTop:20,
+        margin:4,
+   
+        
+    }
+});
