460cd3fab406dea5c9b9be442b2ebafa576a2acb
mariam <>
2022-03-14 14:07:40 +0500

    added flatlist which shows requested services with a button that navigate to a detailed screen for the event and 2 buttons for accepting and rejecting the service

--- ios/rnTest/components/services.js
+++ ios/rnTest/components/services.js
@@ -6,6 +6,7 @@ import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 
 const services = (props) => {
+    console.log('props....',props?.requestedServices)
     const [fetchedServices,setFetchedServices]=useState()
     const [serviceId,setServiceId]=useState()
     useEffect(()=>{
@@ -26,12 +27,16 @@ const services = (props) => {
  console.log('serviceId',serviceId)
  console.log('serv',fetchedServices)  
     return (
-        <View>
+       
+
+     
+      
+        <View >
 
           
                {
                props. services==true?
-                <View>
+                <View >
                    <TouchableOpacity onPress={()=>props?.navigation.navigate('createServices',
                    {
                        service:fetchedServices,
@@ -45,7 +50,41 @@ const services = (props) => {
                        <Icon.Entypo name='plus' size={20} color="black" />
                        </View>
                    </TouchableOpacity>
-                  <Text style={{fontSize:16,fontWeight:'bold',color:'#a16281',justifyContent:'center',alignSelf:'center',marginTop:20}}> Servies</Text>
+                   <Text style={{fontSize:16,fontWeight:'bold',color:'#a16281',justifyContent:'center',alignSelf:'center',marginTop:5}}>Requested Services</Text>
+                 <View style={{}}> 
+                   {props?.requestedServices!==undefined?
+                   
+                   <FlatList
+                    horizontal={false}
+                    data={props?.requestedServices}
+                    keyExtractor={(item,index)=>index.toString()}
+                    renderItem={({item})=>{
+                        console.log('request',item)
+                        return(
+                            <View >
+                                <View style={styles.Input}> 
+                                <TouchableOpacity onPress={()=>props?.navigation?.navigate('reqServdetails',{
+                                    requestedServices:item
+                                })}>
+                                    <View style={{flexDirection:'row',justifyContent:'center'}}>
+                                    <Text style={{fontSize:16,fontWeight:'bold',color:'#a16281',}}>{item.name}/</Text>
+                                    <Text style={{fontSize:16,fontWeight:'bold',color:'#541629',}}>{item.type}</Text>
+                                    </View>
+                                </TouchableOpacity >
+                                <View style={{flexDirection:'row',justifyContent:'center'}}>
+                                <TouchableOpacity style={styles.Button}>
+                                    <Text>Accept</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity  style={styles.Button}>
+                                    <Text>Reject</Text>
+                                </TouchableOpacity>
+                                </View>
+                                </View>
+                            </View>
+                        )
+                    }}
+                   />:null}
+                  <Text style={{fontSize:16,fontWeight:'bold',color:'#a16281',justifyContent:'center',alignSelf:'center',marginTop:20}}>Active Servies</Text>
                   <FlatList
                   horizontal={false}
                   data={fetchedServices}
@@ -53,7 +92,7 @@ const services = (props) => {
                   renderItem={({item})=>{
                       console.log('itemhere',item)
                       return(
-                          <View>
+                          <View >
                               <TouchableOpacity  style={styles.Input} onPress={()=>props?.navigation.navigate('EditServices',{
                                   type:item.type,
                                   price:item.price,
@@ -70,11 +109,16 @@ const services = (props) => {
                   }}
                   />
                     </View>
+                    </View>
                       : null
+                      
             }
+          
 
-            
+        
         </View>
+   
+       
     )
 }
 
@@ -95,4 +139,14 @@ const styles = StyleSheet.create({
       
   
     },
+    Button:{
+        backgroundColor:'#a16281',
+        borderColor:'#a16281',
+        borderWidth:7,
+        alignSelf:'center',
+        marginVertical:5,
+        marginHorizontal:20,
+   
+        
+    }
 })
